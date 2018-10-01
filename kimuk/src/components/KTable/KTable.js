4028d98c0a93a7b5e8bc71c62557fe2d673a6978
Mauro <mauromm1996@gmail.com>
2018-10-01 16:28:28 -0600

    Agrega Filtros, sin visualizacion aun

--- kimuk/src/components/KTable/KTable.js
+++ kimuk/src/components/KTable/KTable.js
@@ -4,13 +4,31 @@ import './KTable.css';
 export default class KTable extends Component {
   constructor(props){
     super(props);
+
+    console.log(this.filterItemsSearchBar('Luis Hidalgo'));
+    console.log(this.filterItemsState('Pendiente'));
+  }
+
+  filterItemsSearchBar = (query) => {
+    return this.props.rows.filter((el) =>
+      el.Nombre.replace(/\s/g,'').toLowerCase().indexOf(query.replace(/\s/g,'').toLowerCase()) > -1 ||
+      el.Primer_apellido.replace(/\s/g,'').toLowerCase().indexOf(query.replace(/\s/g,'').toLowerCase()) > -1 ||
+      el.Segundo_apellido.replace(/\s/g,'').toLowerCase().indexOf(query.replace(/\s/g,'').toLowerCase()) > -1 ||
+      (el.Nombre+el.Primer_apellido+el.Segundo_apellido).replace(/\s/g,'').toLowerCase().indexOf(query.replace(/\s/g,'').toLowerCase()) > -1
+    );
+  }
+
+  filterItemsState = (query) => {
+    return this.props.rows.filter((el) =>
+      el.Estado_solicitud === query
+    );
   }
 
   createTable = () => {
     let table = [];
-
-    
   }
+
+
 
   render() {
     return (
