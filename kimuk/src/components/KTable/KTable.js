4c14cd5e354ca0f450bd84ae2bb1a64a4e92c935
Luis Cascante <luisalonzocascante@gmail.com>
2018-10-04 12:12:00 -0600

    componente de tabla funcional

--- kimuk/src/components/KTable/KTable.js
+++ kimuk/src/components/KTable/KTable.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './KTable.css';
 
+//Imports for pop up
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
+
 export default class KTable extends Component {
   constructor(props){
     super(props);
 
-    console.log(this.filterItemsSearchBar('Luis Hidalgo'));
-    console.log(this.filterItemsState('Pendiente'));
+    //console.log(this.filterItemsSearchBar('Luis Hidalgo'));
+    //console.log(this.filterItemsState('Pendiente'));
   }
 
   filterItemsSearchBar = (query) => {
@@ -24,8 +28,46 @@ export default class KTable extends Component {
     );
   }
 
+  createStateCell = (pState) => {
+    if(pState == 'Pendiente') {
+      return <td className="pendiente">{pState}</td>
+    }
+
+    else if(pState == 'Aprobado'){
+      return <td className="aprobado">{pState}</td>
+    }
+
+    else if(pState == 'No aprobado'){
+      return <td className="no_aprobado">{pState}</td>
+    }
+
+    else if(pState == 'Aprobado para seguro'){
+      return <td className="aprobado_seguro">{pState}</td>
+    }
+  }
+
   createTable = () => {
-    let table = [];
+    let tableBody = [];
+
+    const voluntarios = this.props.rows;
+
+    console.log(voluntarios);
+
+    for(var i = 1; i < voluntarios.length; i++){
+      if(voluntarios[i]){
+        tableBody.push(<tr>
+                        <td></td>
+                        <td>{voluntarios[i].Nombre}</td>
+                        <td>{this.createStateCell(voluntarios[i].Estado_solicitud)}</td>
+                        <td>{voluntarios[i].Ocupacion}</td>
+                        <td>{voluntarios[i].Fecha_registro}</td>
+                        <td><a href="#">Editar Información</a></td>
+                        <td><input type="checkbox"/></td>
+                      </tr>);
+      }
+    }
+
+    return tableBody;
   }
 
 
@@ -35,11 +77,22 @@ export default class KTable extends Component {
       <div className='members_container'>
         <input type='search' className="searchBar" />
         <button type="button">Buscar</button>
-        <div>
-          <table>
-            {this.createTable()}
-          </table>
-        </div>
+
+        <table className="volunteer_table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Nombre completo</th>
+              <th>Estado</th>
+              <th>Ocupación</th>
+              <th>Fecha de registro</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          {this.createTable()}
+        </table>
+
 			</div>
     );
   }
