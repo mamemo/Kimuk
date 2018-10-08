2f7939d7a39e42969292c57311c0943fff0d9f61
Mauro <mauromm1996@gmail.com>
2018-10-08 14:13:00 -0600

    Listo Registrar Voluntario-Luis y Olman

--- kimuk/src/components/DB/abilities.js
+++ kimuk/src/components/DB/abilities.js
@@ -6,7 +6,7 @@ export {insertar_actualizar_habilidades, actualizar_key_habilidades, eliminar_ha
 function insertar_actualizar_habilidades(Id, nombre) {
     firebase.database().ref('Habilidades/' + Id).update({
         Nombre: nombre
-    });
+    }, function (error) {return error;});
 }
 
 
@@ -14,22 +14,23 @@ function actualizar_key_habilidades(Id_habilidad, Id_nuevo) {
     const ref = firebase.database().ref('Habilidades/');
     const child = ref.child(Id_habilidad);
     child.once('value', function (snapshot) {
-        ref.child(Id_nuevo).set(snapshot.val());
-        child.remove();
+        ref.child(Id_nuevo).set(snapshot.val(), function (error) {return error;});
+        child.remove(function (error) {return error;});
     });
 }
 
 
 function eliminar_habilidades(Id_habilidad) {
     const ref = firebase.database().ref('Habilidades');
-    ref.child(Id_habilidad).remove();
+    ref.child(Id_habilidad).remove(function (error) {return error;});
 }
 
 
 function leer_habilidades() {
+    return new Promise(resolve => {
     const ref = firebase.database().ref("Habilidades");
     ref.on("value", function (snapshot) {
-        console.log(snapshot.val());
-        return snapshot.val();
+       resolve(snapshot.val());
+    });
     });
 }
--- kimuk/src/components/KFormVoluntario/KTeryCon.js
+++ kimuk/src/components/KFormVoluntario/KTeryCon.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './KFormVoluntario.css';
 import '../style/color.css';
-import '../DB/abilities';
-import {insertar_actualizar_habilidades} from "../DB/abilities";
+import {insertar_actualizar_voluntarios_camapana, insertar_actualizar_habilidades_voluntarios,
+    actualizar_voluntarios_campana, eliminar_voluntario} from '../DB/volunteers';
 
 export default class KTeryCon extends Component {
     constructor(){
@@ -11,9 +11,24 @@ export default class KTeryCon extends Component {
         this.guardar_info=this.guardar_info.bind(this);
     }
     registrar(){
-        console.log(this.props.voluntario);
-        insertar_actualizar_habilidades(800, "Luis");
-        //codigo del registro
+        const d = new Date();
+        insertar_actualizar_voluntarios_camapana(this.props.voluntario.Id_campana,
+            this.props.voluntario.id,this.props.voluntario.tipo_id,this.props.voluntario.nombre,
+            this.props.voluntario.apellido_1,this.props.voluntario.apellido_2,
+            this.props.voluntario.f_nacimiento,this.props.voluntario.genero,
+            this.props.voluntario.estado_civil,this.props.voluntario.ocupacion,
+            this.props.voluntario.provincia,this.props.voluntario.canton,this.props.voluntario.distrito,
+            this.props.voluntario.direccion_exacta,this.props.voluntario.correo,
+            this.props.voluntario.telefono_1,this.props.voluntario.telefono_2,
+            d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear(),"Pendiente")
+
+        for (let habilidad in this.props.voluntario.habilidades) {
+            if (insertar_actualizar_habilidades_voluntarios(this.props.voluntario.Id_campana,
+                this.props.voluntario.id, habilidad, this.props.voluntario.habilidades[habilidad])) {
+                alert("Ha ocurrido un error al registrar el voluntario");
+                return;
+            }
+        }
     }
     guardar_info(e){
         this.props.voluntario[e.target.name]=e.target.value;
@@ -22,24 +37,28 @@ export default class KTeryCon extends Component {
         return(
             <div className="container">
                 <div className="row"> 
-                <div className="col-6 offset-3">
+                <div className="col-6 offset-3 border border-dark text-justify">
                 {this.props.tyc}
                 </div>
                 </div>
                 <div className="row"> 
-                    <div className="col-4 offset-6">
+                    <div className="col-4 offset-6 text-left">
                     <input type="radio" name="tyc" value="si" defaultChecked={this.props.voluntario.tyc==="si"} onChange={this.guardar_info}/> Acepto términos y condiciones <br/>
                     <input type="radio" name="tyc" value="no" defaultChecked={this.props.voluntario.tyc==="no"} onChange={this.guardar_info}/> No acepto <br/>
+                    <br/>
+                    <br/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-1 offset-2">
-                        <button className="bt" onClick={ this.props.anterior }>anterior</button> 
+                        <button className="btn btn-info" onClick={ this.props.anterior }>anterior</button> 
                     </div>
                     <div className="col-1 offset-6">
-                        <button className="bt-lg" onClick={ this.registrar }>Confirmar</button> 
+                        <button className="btn btn-success" onClick={ this.registrar }>Confirmar</button> 
                     </div>
                 </div>
+                <br/>
+                <br/>
             </div>    
         );
     }
