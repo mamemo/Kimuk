ecc7167c1b5bbd6cd1ec16509e340c564990669b
CarlosMarioSom <krlosmario2805@hotmail.com>
2018-11-05 10:52:10 -0600

    Fixing bugs -> CSS
    - CSS a pop up
    - Acomodo de estructura a documentos y TeryCon para CSS

--- kimuk/src/components/KModals/KModalInfoAccion.js
+++ kimuk/src/components/KModals/KModalInfoAccion.js
@@ -9,10 +9,7 @@ const customStyles = {
         left                  : '50%',
         right                 : 'auto',
         bottom                : 'auto',
-        height                : '500px',
-        width                 : '700px',
         transform             : 'translate(-50%, -50%)',
-        overflow              : 'auto'
     }
 };
 
@@ -64,35 +61,23 @@ export default class KModalHabilidades extends Component
                     isOpen={this.state.modalIsOpen}
                     onRequestClose={this.closeModal}
                     style={customStyles}>
-                    <h6>{this.props.tipoNotificacion}</h6>
-                    <br/>
-                    <h4>{this.props.tipoNotificacion}</h4>
+                    <h4 className="card-header">{this.props.tipoNotificacion}</h4>
                     <br/> <br/>
-                    
                     { this.state.mensajeUsuario }
-
                     <br/> <br/>
-                    <button
-                        className="inside"
-                        data-tip data-for='btn-tooltip-Aceptar'
-                        onClick={() => {
-                            this.closeModal();
-                            if (this.props.tipoNotificacion.indexOf("Error") === -1) {
-                                window.location.href ="http://localhost:3000";
-                            }
-                        }}
-                    >
-                        Aceptar
-                    </button>
-                    <button
-                        type="button"
-                        className="close"
-                        aria-label="Close"
-                        data-tip data-for='btn-tooltip-CancelarX'
-                        onClick={this.closeModal}
-                    >
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <div className={"container_modalInfoAccion"}>
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => {
+                                this.closeModal();
+                                if (this.props.tipoNotificacion.toLowerCase().indexOf("error") === -1) {
+                                    window.location.href ="http://localhost:3000";
+                                }
+                            }}
+                        >
+                                Aceptar
+                            </button>
+                    </div>
                 </Modal>
             </div>
         );
